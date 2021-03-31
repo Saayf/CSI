c421e5bfae230f1ba672ce5944124f0d6b93a136
Saif Jibraeel <46442644+Saayf@users.noreply.github.com>
2021-03-31 13:14:11 +0200

    Add files via upload

---
+++ help.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Creative - Start Bootstrap Theme</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css"/>
+        <!-- Third party plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/CSI.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                    <img id="logo" src="res/logo_200x200.png" class="bg-white rounded-circle">
+                </a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Login <i class="fas fa-sign-in-alt" style="font-size: 18px"></i></a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Help <i class="fas fa-question" style="font-size: 18px"></i></a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Clothes <i class="fas fa-tshirt" style="font-size: 18px"></i></a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Cart <i class="fas fa-shopping-cart" style="font-size: 18px"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="helpBack">
+            <div class="container h-100">
+                <div id="helpbox" class="row align-items-center justify-content-center text-center">
+                    <form method="post" name="formHelp">
+                        <h3 class="text-white-75 font-weight-light">Email<h3>
+                            <input id="helpEmail" type="email" name="emailHelp">
+                            <br>
+                            <h3 class="text-white-75 font-weight-light">Describe your problem</h3>
+                            <textarea id="helpText" name="textHelp" cols="40" rows="5"></textarea>
+                            <br>
+                            <br>
+                            <input class="btn btn-secondary" value="Send" type="submit" name="submitHelp">
+                    </form>
+                </div>
+            </div>
+        </header>
+        <!-- Footer-->
+        <footer class="bg-light py-5">
+            <div class="container"><div class="small text-center text-muted">Copyright © 2021 - CSI</div></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
